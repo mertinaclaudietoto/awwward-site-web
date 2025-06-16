981b2be6db8e9de1ec6a9f42b6780110f595c082
AlinaMertina <mertinaclaudietoto@gmail.com>
2025-06-16 11:43:46 +0300

    initilisation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import './index.css'
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl font-bold text-yellow-500">Welcome to Awwwards</h1>
+    </main>
+  )
+}
+
+export default App
+// 8:42
